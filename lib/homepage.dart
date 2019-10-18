6ec17ac362021eb0f024856a8e01e752eb1a19af
Remy Nguyen <remynguyen96@gmail.com>
2019-10-19 04:54:07 +0700

    Configuration build local device on IOS

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 
-//import 'login/login_view.dart';
-//import 'navigation/FirstStep.dart';
-//import 'navigation/NavigateParams.dart';
-//import 'navigation/NavigationData.dart';
-import 'navigation/NavigationData2.dart';
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Navigation with Arguments',
+      title: 'State Management',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Color(0xff388E3C),
@@ -23,34 +17,12 @@ class MyApp extends StatelessWidget {
           body1: TextStyle(fontSize: 16),
         ),
       ),
-
-      /// General Navigation
-//      initialRoute: '/',
-//      routes: {
-//        '/': (context) => FirstStep(),
-//        '/second': (context) => SecondStep(),
-//      },
-
-      /// Navigation with parameters
-//      onGenerateRoute: (settings) {
-//        if (settings.name == PassArgumentsScreen.routeName) {
-//          final ScreenArguments args = settings.arguments;
-//
-//          return MaterialPageRoute(
-//            builder: (context) => PassArgumentsScreen(
-//              title: args.title,
-//              message: args.message,
-//            ),
-//          );
-//        }
-//
-//        return null;
-//      },
-
-      home: HomeScreen(),
-
-//      home: FirstStep(),
-//      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple State Management'),
+        ),
+        body: Container(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/homepage.dart';
-//import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import './homepage.dart';
 
 void main() {
-//  debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
